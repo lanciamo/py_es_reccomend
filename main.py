7bc7b0a7ad86e513e18336489217bddc60e2c389
Leonid Kashin <lkashin@gmail.com>
2020-02-02 23:43:58 +0400

    #google

--- main.py
+++ main.py
@@ -151,16 +151,16 @@ def recomended_for(userId):
     return res_fids
 
 
-# function for responses
-def results():
-    # build a request object
-    req = request.get_json(force=True)
-
-    # fetch action from json
-    action = req.get('queryResult').get('action')
-
-    # return a fulfillment response
-    return {'fulfillmentText': 'This is a response from webhook for Reccomends'}
+# # function for responses
+# def results():
+#     # build a request object
+#     req = request.get_json(force=True)
+#
+#     # fetch action from json
+#     action = req.get('queryResult').get('action')
+#
+#     # return a fulfillment response
+#     return {'fulfillmentText': 'This is a response from webhook for Reccomends'}
 
 
 user_id_lib = {}
