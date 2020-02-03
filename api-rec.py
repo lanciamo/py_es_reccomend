879016c47edb15ac9516805e3fd89d0398ec9ea3
Leonid Kashin <lkashin@gmail.com>
2020-02-03 17:24:00 +0400

    main.py debug menu3

--- api-rec.py
+++ api-rec.py
@@ -158,9 +158,9 @@ def user_preffers(userId):
     return output
 
 
-def recomended_for(userId):
-    preffer = user_preffers(userId)
-    res_fids = search_fids(preffer, userId)
+def recomended_for(user_id):
+    preffer = user_preffers(user_id)
+    res_fids = search_fids(str(preffer), user_id)
     return res_fids
 
 
