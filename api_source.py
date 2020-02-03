434c90ee54110f74343f5cac516df24f7d14c1e1
Leonid Kashin <lkashin@gmail.com>
2020-02-03 21:39:40 +0400

    новый экс в source

--- api_source.py
+++ api_source.py
@@ -1,21 +1,26 @@
 # coding: utf-8
 # Импортирует поддержку UTF-8.
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
 todos = {}
 
+parser = reqparse.RequestParser()
+parser.add_argument('uid')
+
 
 class TodoSimple(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+    def get(self):
+        args = parser.parse_args()
+        return {args: args}
 
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
+    # def put(self, todo_id):
+    #     todos[todo_id] = request.form['data']
+    #     return {todo_id: todos[todo_id]}
+
 
 
 api.add_resource(TodoSimple, '/<string:todo_id>')
