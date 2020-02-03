ac8d1452a2034c49f46265b8e203144c94c7466a
Leonid Kashin <lkashin@gmail.com>
2020-02-03 22:06:38 +0400

    новый экс в source

--- api_source.py
+++ api_source.py
@@ -23,7 +23,7 @@ class TodoSimple(Resource):
     #     return {todo_id: todos[todo_id]}
 
 
-api.add_resource(parser, '/')
+api.add_resource(TodoSimple, '/', '/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
