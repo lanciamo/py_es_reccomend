935700d9eb761478f525a61040f341de640d43d8
Leonid Kashin <lkashin@gmail.com>
2020-02-03 16:16:33 +0400

    api-rec.py(new!)

--- api-rec.py
+++ api-rec.py
@@ -7,152 +7,16 @@ import random
 app = Flask(__name__)
 api = Api(app)
 
-ai_quotes = [
-    {
-        "id": 0,
-        "author": "Kevin Kelly",
-        "quote": "The business plans of the next 10,000 startups are easy to forecast: " +
-                 "Take X and add AI."
-    },
-    {
-        "id": 1,
-        "author": "Stephen Hawking",
-        "quote": "The development of full artificial intelligence could " +
-                 "spell the end of the human race… " +
-                 "It would take off on its own, and re-design " +
-                 "itself at an ever increasing rate. " +
-                 "Humans, who are limited by slow biological evolution, " +
-                 "couldn't compete, and would be superseded."
-    },
-    {
-        "id": 2,
-        "author": "Claude Shannon",
-        "quote": "I visualize a time when we will be to robots what " +
-                 "dogs are to humans, " +
-                 "and I’m rooting for the machines."
-    },
-    {
-        "id": 3,
-        "author": "Elon Musk",
-        "quote": "The pace of progress in artificial intelligence " +
-                 "(I’m not referring to narrow AI) " +
-                 "is incredibly fast. Unless you have direct " +
-                 "exposure to groups like Deepmind, " +
-                 "you have no idea how fast — it is growing " +
-                 "at a pace close to exponential. " +
-                 "The risk of something seriously dangerous " +
-                 "happening is in the five-year timeframe." +
-                 "10 years at most."
-    },
-    {
-        "id": 4,
-        "author": "Geoffrey Hinton",
-        "quote": "I have always been convinced that the only way " +
-                 "to get artificial intelligence to work " +
-                 "is to do the computation in a way similar to the human brain. " +
-                 "That is the goal I have been pursuing. We are making progress, " +
-                 "though we still have lots to learn about " +
-                 "how the brain actually works."
-    },
-    {
-        "id": 5,
-        "author": "Pedro Domingos",
-        "quote": "People worry that computers will " +
-                 "get too smart and take over the world, " +
-                 "but the real problem is that they're too stupid " +
-                 "and they've already taken over the world."
-    },
-    {
-        "id": 6,
-        "author": "Alan Turing",
-        "quote": "It seems probable that once the machine thinking " +
-                 "method had started, it would not take long " +
-                 "to outstrip our feeble powers… " +
-                 "They would be able to converse " +
-                 "with each other to sharpen their wits. " +
-                 "At some stage therefore, we should " +
-                 "have to expect the machines to take control."
-    },
-    {
-        "id": 7,
-        "author": "Ray Kurzweil",
-        "quote": "Artificial intelligence will reach " +
-                 "human levels by around 2029. " +
-                 "Follow that out further to, say, 2045, " +
-                 "we will have multiplied the intelligence, " +
-                 "the human biological machine intelligence " +
-                 "of our civilization a billion-fold."
-    },
-    {
-        "id": 8,
-        "author": "Sebastian Thrun",
-        "quote": "Nobody phrases it this way, but I think " +
-                 "that artificial intelligence " +
-                 "is almost a humanities discipline. It's really an attempt " +
-                 "to understand human intelligence and human cognition."
-    },
-    {
-        "id": 9,
-        "author": "Andrew Ng",
-        "quote": "We're making this analogy that AI is the new electricity." +
-                 "Electricity transformed industries: agriculture, " +
-                 "transportation, communication, manufacturing."
-    }
-]
+queries = {}
 
 
-class Quote(Resource):
-    def get(self, userid=0):
-        if userid == 0:
-            return 'Please send userid', 404
-            # return random.choice(ai_quotes), 200
-        else:
-            # for quote in ai_quotes:
-            #     if quote["id"] == id:
-            #         return quote, 200
-            # return "Quote not found", 404
-            return recomended_for(userid), 200
+class Recommend(Resource):
+    def get(self, user_id):
+        return {"id": user_id}
 
-    # def post(self, id):  # добавление
-    #     parser = reqparse.RequestParser()
-    #     parser.add_argument("author")
-    #     parser.add_argument("quote")
-    #     params = parser.parse_args()
-    #     for quote in ai_quotes:
-    #         if id == quote["id"]:
-    #             return f"Quote with id {id} already exists", 400
-    #     quote = {
-    #         "id": int(id),
-    #         "author": params["author"],
-    #         "quote": params["quote"]
-    #     }
-    #     ai_quotes.append(quote)
-    #     return quote, 201
-    #
-    # def put(self, id):  # изменения
-    #     parser = reqparse.RequestParser()
-    #     parser.add_argument("author")
-    #     parser.add_argument("quote")
-    #     params = parser.parse_args()
-    #     for quote in ai_quotes:
-    #         if (id == quote["id"]):
-    #             quote["author"] = params["author"]
-    #             quote["quote"] = params["quote"]
-    #             return quote, 200
-    #
-    #     quote = {
-    #         "id": id,
-    #         "author": params["author"],
-    #         "quote": params["quote"]
-    #     }
-    #
-    #     ai_quotes.append(quote)
-    #     return quote, 201
-    #
-    # def delete(self, id):
-    #     global ai_quotes
-    #     ai_quotes = [qoute for qoute in ai_quotes if qoute["id"] != id]
-    #     return f"Quote with id {id} is deleted.", 200
+    def put(self, user_id):
+        queries[user_id] = request.form['data']
+        return {queries: queries[user_id]}
 
 
 def delete_index(name_index):
@@ -295,7 +159,6 @@ def recomended_for(userId):
     return res_fids
 
 
-# api.add_resource(Quote, "/ai-quotes", "/ai-quotes/", "/ai-quotes/<int:id>")
-api.add_resource(Quote, "/recc", "/recc/", "/recc/<int:userid>")
+api.add_resource(Recommend, '/<string:todo_id>')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
