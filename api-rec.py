ed40e3f1de1eab3607d20f3baa5914994310faf6
Leonid Kashin <lkashin@gmail.com>
2020-02-06 00:15:25 +0400

    with difference in results

--- api-rec.py
+++ api-rec.py
@@ -92,22 +92,19 @@ def search_fids(fids, userId):
                     body={
                         "size": 10,
                         "_source": ["cId"],
-                        "sort": [{"date": "desc"}],
                         "query": {
                             "bool": {
                                 "should": [
                                     {
-                                        "multi_match": {
-                                            "query": str(fids),
-                                            "fields": ["fids"]
+                                        "match": {
+                                            "fids": fids
                                         }
                                     }
                                 ],
                                 "must_not": [
                                     {
-                                        "multi_match": {
-                                            "query": int(userId),
-                                            "fields": ["userId"]
+                                        "term": {
+                                            "userId": userId
                                         }
                                     }
                                 ]
@@ -122,6 +119,14 @@ def search_fids(fids, userId):
         a1[i] = res['hits']['hits'][i]['_source']['cId']
     res = json.dumps(a1)
     return res
+
+
+def list_to_txt(lis):
+    b = ''
+    for i in range(0, len(lis)):
+        b = b + str(lis[i]) + ' '
+    b = remove(b, '\,/:*?"<[]>|')
+    return b
 
 
 def search_tags_from_userId(userId):
@@ -143,7 +148,7 @@ def search_tags_from_userId(userId):
 def remove(value, deletechars):
     for c in deletechars:
         value = value.replace(c, '')
-    return value
+    return value;
 
 
 def most_popular_fids():
@@ -157,45 +162,36 @@ def most_popular_fids():
     a = ''
     for i in range(0, len(res['hits']['hits'])):
         a = a + res['hits']['hits'][i]['_source']['fids'] + ' '
-    a = remove(a, '\,/:*?"<[]>|')
+    #     a = remove(a, '\,/:*?"<[]>|')
     counts = Counter(a.split(' ')[:-1])
     d = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
     output = []
     for k in d:
         if k not in output:
             output.append(k)
-            if len(output) == 20:
+            if len(output) == 10:
                 break
     return output
 
 
-def list_to_txt(lis):
-    b = ''
-    random.shuffle(lis)
-    for i in range(0, len(lis)):
-        b = b + lis[i] + ' '
-    b = remove(b, '\,/:*?"<[]>|')
-    return b
-
-
 def user_preffers(userId):
     res = search_tags_from_userId(userId)
-    a = ''
-    for i in range(0, len(res['hits']['hits'])):
-        a = a + res['hits']['hits'][i]['_source']['fids'] + ' '
-    a = remove(a, '\,/:*?"<[]>|')
-    counts = Counter(a.split(' ')[:-1])
-    d = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
-    output = []
-    for k in d:
-        if k not in output:
-            output.append(k)
-            if len(output) == 20:
-                break
-    if len(output) < 2:
+    if len(res['hits']['hits']) == 0:
         mpf = most_popular_fids()
         return mpf
     else:
+        a = ''
+        for i in range(0, len(res['hits']['hits'])):
+            a = a + res['hits']['hits'][i]['_source']['fids'] + ' '
+        #         a = remove(a, '\,/:*?"<[]>|')
+        counts = Counter(a.split(' ')[:-1])
+        d = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
+        output = []
+        for k in d:
+            if k not in output:
+                output.append(k)
+                if len(output) == 10:
+                    break
         return output
 
 
@@ -204,6 +200,7 @@ def recomended_for(userId):
     preffer = list_to_txt(preffer)
     res_fids = search_fids(preffer, userId)
     return res_fids
+
 
 api.add_resource(Events, '/')
 if __name__ == '__main__':
