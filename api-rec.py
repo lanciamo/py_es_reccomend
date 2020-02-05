a4ab21b4a18dbd0400a2faddfee12b005ebdee30
Leonid Kashin <lkashin@gmail.com>
2020-02-06 00:37:01 +0400

    with difference in results

--- api-rec.py
+++ api-rec.py
@@ -28,62 +28,6 @@ class Events(Resource):
         return recomended_for(args['uid']), 201
 
 
-def delete_index(name_index):  # ТОЛЬКО ДЛЯ УДАЛЕНИЯ
-    es.indices.delete(index=name_index, ignore=400)
-
-
-def new_index(name_index):
-    # mapping dictionary that contains the settings and
-    # _mapping schema for a new Elasticsearch index:
-    mapping = {
-        "settings": {
-            "number_of_shards": 1,
-            "number_of_replicas": 1
-        },
-        "mappings": {
-            "properties": {
-                "userId": {
-                    "type": "integer"  # formerly "string"
-                },
-                "date": {
-                    "type": "date",
-                    "format": "yyyy-MM-dd HH:mm:ss"
-                },
-                "cId": {
-                    "type": "integer"
-                },
-                "action": {
-                    "type": "text"
-                },
-                "fids": {
-                    "type": "text"
-                }
-            }
-        }
-    }
-
-    # ТОЛЬКО ДЛЯ СОЗДАНИЯ
-    # create an index in elasticsearch, ignore status code 400 (index already exists)
-    es.indices.create(index=name_index, body=mapping)
-    #    {'acknowledged': True, 'shards_acknowledged': True, 'index': 'my-index'}
-
-
-def info_index(name_index):  # get info about index
-    es.indices.get_mapping(index=name_index)
-
-
-def load_info():
-    f = open('treto01.json')
-    data = json.loads(f.read())
-    dict_for_downloads = {i: data[i] for i in range(0, len(data))}
-    for id, item in dict_for_downloads.items():
-        item['fids'] = str(item['fids'])
-    for id, item in dict_for_downloads.items():
-        es.index(index="events", id=id, doc_type='_doc', body=item)
-    #     if id > 2:
-    #         break
-
-
 # ==================
 
 
@@ -148,7 +92,7 @@ def search_tags_from_userId(userId):
 def remove(value, deletechars):
     for c in deletechars:
         value = value.replace(c, '')
-    return value;
+    return value
 
 
 def most_popular_fids():
